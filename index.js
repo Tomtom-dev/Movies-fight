bb476da0e4a846c1bd7c9d31dd3c79f787ba3df7
Tomtom-dev <thomasheddadji0@gmail.com>
2021-02-12 13:08:11 +0100

    test to fetch the data from the api with the api key

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const fetchData = async () =>{
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params:{
+            apikey:"a7d4986a",
+            s:'avengers'
+        }
+    });
+    console.log(response.data);
+}
+
+fetchData()
