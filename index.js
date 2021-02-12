c5a21b6a9bf9d21da42d7555b6ee9edafdb1cf85
Tomtom-dev <thomasheddadji0@gmail.com>
2021-02-12 15:06:49 +0100

    refactoring functions

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ const fetchData = async (searchTerm) =>{
 
 const input = document.querySelector("input")
 
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId){
+            clearTimeout(timeoutId)
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    }
+}
+
 let timeoutId;
 const onInput = event => {
-    if(timeoutId){
-        clearTimeout(timeoutId)
-    }
-    timeoutId = setTimeout(() => {
         fetchData(event.target.value)
-    }, 1000);
 };
 
-input.addEventListener('input' , onInput)
+input.addEventListener('input' , debounce(onInput,500))
