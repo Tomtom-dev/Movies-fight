980199c678e46dd4739b4c64eadda54c17b71fc6
Tomtom-dev <thomasheddadji0@gmail.com>
2021-02-15 23:07:00 +0100

    add ternary option to display a picture or blank if no picture

--- index.js
+++ index.js
@@ -28,14 +28,16 @@ const resultsWrapper = document.querySelector(".results");
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value)
-    // console.log(movies);
+
+    resultsWrapper.innerHTML=''
     dropdown.classList.add('is-active');
     for (let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? "" : movie.Poster
 
         option.classList.add('dropdown-item')
         option.innerHTML= `
-        <img src="${movie.Poster}" />
+        <img src="${imgSrc}" />
         ${movie.Title}
         `;
 
